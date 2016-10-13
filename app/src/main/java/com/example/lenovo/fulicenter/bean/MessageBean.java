b8f8217eb48df525a4d0a7fddb33ff3fe6878212
刘宁 <1039212799@qq.com>
2016-10-14 02:55:56 +0800

    添加bean包的实体类

---
+++ app/src/main/java/com/example/lenovo/fulicenter/bean/MessageBean.java
@@ -0,0 +1,42 @@
+package com.example.lenovo.fulicenter.bean;
+
+/**
+ * Created by xiaomiao on 2016/10/14.
+ */
+public class MessageBean {
+
+    /**
+     * success : true
+     * msg : 收藏成功
+     */
+
+    private boolean success;
+    private String msg;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public MessageBean() {
+    }
+
+    @Override
+    public String toString() {
+        return "MessageBean{" +
+                "success=" + success +
+                ", msg='" + msg + '\'' +
+                '}';
+    }
+}
